dad8649a62184e6fd841e5b620393e8082b90c0a
Carlos <carlosdummy14@gmail.com>
2024-02-22 21:18:40 -0600

    add strictMode

--- src/main.tsx
+++ src/main.tsx
@@ -14,10 +14,10 @@ const darkTheme = createTheme({
 })
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
